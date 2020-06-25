583670f0c9ba8d81bd6afa8d13b3862db00f9a58
Neha Sebastian <nehasebastian@MacBook-Pro.local>
2020-06-24 21:55:52 -0400

    Added my name

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+<<<<<<< HEAD
 HEAD
 
 var
@@ -6,3 +7,15 @@ var
 Siddharth Shah
 
  caafcee75a05673d701859c2f384c129f36cd55d
+=======
+/*
+ Everyone please create a branch using 
+ git branch <branch_name>
+ then 
+ git checkout branch 
+
+ or short cut
+ git checkout -b <branchname> which will create and checkout the brnach
+ add your name with line number in file names.txt
+*/
+>>>>>>> 26e8b44e7386acfe4068ad689936ef0ab5aefc10
